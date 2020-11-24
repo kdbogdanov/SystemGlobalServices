8a498d5fc25e97dc5bd812d609a5f3fa445e6359
kdbogdanov <Furious1391>
2020-11-25 01:25:43 +0300

    some changes

--- WebApplication/Controllers/CurrenciesController.cs
+++ WebApplication/Controllers/CurrenciesController.cs
@@ -2,6 +2,7 @@
 using WebApplication.Services;
 using WebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace WebApplication.Controllers
 {
@@ -9,21 +10,18 @@ namespace WebApplication.Controllers
     public class CurrenciesController : Controller
     {
         private readonly ICurrencyService _currencyService;
-        private readonly CurrenciesResponse _currencies;
 
-        public CurrenciesController(ICurrencyService currencyService)
-        {
+        public CurrenciesController(ICurrencyService currencyService) =>
             _currencyService = currencyService;
-            _currencies = _currencyService.GetCurrenciesAsync().Result;
-        }
-            
+
 
         // GET: Currencies
         [HttpGet]
         [Route("api/currencies")]
-        public ActionResult GetCurrencies(int page = 1, int pageSize = 3)
+        public async Task<ActionResult> GetCurrenciesAsync(int page = 1, int pageSize = 3)
         {
-            var source = _currencies.Valute.Values.ToList();
+            var currencies = await _currencyService.GetCurrenciesAsync();
+            var source = currencies.Valute.Values.ToList();
             var items = source.Skip((page - 1) * pageSize).Take(pageSize);
 
             return Ok(items);
@@ -32,12 +30,13 @@ namespace WebApplication.Controllers
         // GET: Currency/
         [HttpGet]
         [Route("api/currency/{id?}")]
-        public ActionResult GetCurrency(string id = "R01235")
+        public async Task<ActionResult> GetCurrencyAsync(string id = "R01235")
         {
+            var currencies = await _currencyService.GetCurrenciesAsync();
             decimal result = 0;
-            foreach (var key in _currencies.Valute.Keys)
-                if (_currencies.Valute[key].ID == id)
-                    result = _currencies.Valute[key].Value;
+            foreach (var key in currencies.Valute.Keys)
+                if (currencies.Valute[key].ID == id)
+                    result = currencies.Valute[key].Value;
 
             return Ok(result);
         }
